6564b6af690bcd023f3d6a7c13c3cf126699e5cf
Greaper88 <greaper.9.88@gmail.com>
2022-01-07 03:54:47 -0600

    Renamed Build to Compile, Created alternate build batch file.

---
+++ source/compile-kelp-as-fuel.bat
@@ -0,0 +1,8 @@
+ECHO OFF
+CLS
+ECHO Thise program will generate the datapack based off the config.json file.
+ECHO.
+ECHO Please ensure that you have installed NodeJS before attempting to run this.
+PAUSE
+node generate-kelp-as-fuel.js
+PAUSE
